e1a006c45428763d1009e17865bbbd22158901e6
malik <mha2448@wgu.edu>
2024-03-01 16:25:48 -0500

    Added edge case to account for incorrect addresses on packages in Package.py

--- Package.py
+++ Package.py
@@ -1,3 +1,4 @@
+import datetime
 class Package:
     def __init__(self, ID, address, city, state, zipcode, Deadline_time, weight, status):
         self.ID = ID
@@ -11,11 +12,12 @@ class Package:
         self.departure_time = None
         self.delivery_time = None
 
+
+    # Learned how to do these comments here https://www.w3schools.com/python/python_comments.asp
     def __str__(self):
         """Return a string representation of the package."""
         return (f"{self.ID}, {self.address}, {self.city}, {self.state}, {self.zipcode}, "
                 f"{self.Deadline_time}, {self.weight}Kg, Delivered at: {self.delivery_time}, Status: {self.status}")
-
 
     def update_status(self, convert_timedelta):
         if self.delivery_time < convert_timedelta:
@@ -24,3 +26,13 @@ class Package:
             self.status = "En route"
         else:
             self.status = "At Hub"
+        if self.ID == 9:  # Added Edge case that will change the address for package 9 to the correct address once it's been received
+                if convert_timedelta > datetime.timedelta(hours=10, minutes=20):
+                    self.address = "410 S State St"
+                    self.zipcode = "84111"
+                else:
+                    self.address = "300 State St"
+                    self.zipcode = "84103"
+
+
+
